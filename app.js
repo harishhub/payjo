b6de9290aebeb26a12748b5155cfef432c6c15b6
harishhub_8310 <hk8049@gmail.com>
2017-07-09 22:52:23 +0530

    removed comments and unwanted files

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-/*<<<<<<< HEAD
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var products = require('./routes/products');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-=======*/
-
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
